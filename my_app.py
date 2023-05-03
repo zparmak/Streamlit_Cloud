62c1b4139c4e8f25952cc6b5e49e6f8b23acabb3
Zeynep Parmak <116037740+zparmak@users.noreply.github.com>
2023-05-03 17:21:45 +0300

    Add files via upload

---
+++ my_app.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from PIL import Image
+
+st.title("This is a title")
+st.text("This is some text")
+
+#Markdovn
+st.markdown("Streamlit is **_really_cool** :+1:")
+st.markdown("#This is a markdown")
+st.markdown("## This is a markdown")
+st.markdown("### This is a markdown")
+
+#Header/Subheader
+st.header("This is a header")
+st.subheader("This is a header")
+
+#Success/Info
+st.success("This is a success message")
+st.info("This is a informational message")
+streamlit run c:/Users/prmkz/OneDrive/Masaüstü/streamlit/my_app.py
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+
